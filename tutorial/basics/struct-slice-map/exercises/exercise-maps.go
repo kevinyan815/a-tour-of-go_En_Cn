f3eb73d8b309fe3dc8e4ba1457d5c1f124d5bd61
Kevin Yan <kevinyan815@gmail.com>
2019-09-26 16:29:34 +0800

    Create exercise-maps.go

---
+++ tutorial/basics/struct-slice-map/exercises/exercise-maps.go
@@ -0,0 +1,23 @@
+
+// Remember to `go get code.google.com/p/go-tour/wc` first
+
+package main
+
+import (
+	"code.google.com/p/go-tour/wc"
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	ret := make(map[string]int)
+	for _, word := range strings.Fields(s) {
+		ret[word] += 1
+	}
+	return ret
+}
+
+func main() {
+	wc.Test(WordCount)
+	fmt.Println()
+}
