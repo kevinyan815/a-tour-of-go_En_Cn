2b8bb99a23817cf102d17250a664632824d3bebc
Kevin Yan <kevinyan815@gmail.com>
2019-10-10 12:50:17 +0800

    Create exercise-stringer.go

---
+++ tutorial/methods-interfaces/exercises/exercise-stringer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "strconv"
+)
+
+type IPAddr [4]byte
+
+func (p IPAddr) String() string {
+    var ipParts []string
+    for _, item := range p {
+    // can use string(item) or string(int(item))
+    // string will convert int or byte to their ASCII presentation
+    // not literal numeric string like "0"
+	ipParts = append(ipParts, strconv.Itoa(int(item)))
+    }
+    return strings.Join(ipParts, ".")
+}
+
+func main() {
+    hosts := map[string]IPAddr{
+	"loopback":  {127, 0, 0, 1},
+	"googleDNS": {8, 8, 8, 8},
+    }
+    for name, ip := range hosts {
+	fmt.Printf("%v: %v\n", name, ip)
+    }
+}
